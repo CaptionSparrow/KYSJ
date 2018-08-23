efff405554ed3fe3ab9de5ae49c8b43825af6df6
captionsparrow <xiongzhq1997@gmail.com>
2018-08-23 18:38:27 +0800

    change station's size

--- simulation/Visualize.py
+++ simulation/Visualize.py
@@ -1,20 +1,21 @@
-# https://blog.csdn.net/theonegis/article/details/51037850
-# http://www.labri.fr/perso/nrougier/teaching/matplotlib/
-# https://matplotlib.org/api/animation_api.html
-
 ''' This file is used to generate animation'''
 
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
-SIZE_MIN = 20
-SIZE_MAX = 20 ** 2
-LINE_MIN = 0.2
-LINE_MAX = 2.0
+SIZE_MIN = 50
+SIZE_MAX = 500
+SIZE_INI = 1000
+LINE_MIN = 0.2 # 0
+LINE_MAX = 2.0 # 10
 NS = 4
 
-pos = np.random.uniform(0, 1, (NS, 2))
+INIT = 500
+DATAS = []
+
+# pos = np.random.uniform(0, 1, (NS, 2))
+pos = np.array([[0.76,0.66], [0.23,0.48], [0.31,0.12], [0.46,0.83]])
 color = np.ones((NS, 4)) * (0, 0, 0, 1)
 color[:,0] = np.linspace(0, 0.5, NS)
 color[:,1] = np.linspace(0, 0.8, NS)
@@ -32,13 +33,15 @@ def generate_lines(ax):
     return lines
 
 def init():
-    global size, scat, line
+    global size, scat, line, time, day
+
+    # print(pos)
 
     # New axis over the whole figure, no frame and a 1:1 aspect ratio
     ax = fig.add_axes([0, 0, 1, 1], frameon = False, aspect = 1)
 
     # Ring sizes
-    size = np.linspace(SIZE_MIN, SIZE_MAX, NS)
+    size = np.linspace(SIZE_INI, SIZE_INI, NS)
 
     # Scatter plot
     scat = ax.scatter(pos[:,0], pos[:,1], s=size, facecolors=color)
@@ -48,26 +51,41 @@ def init():
     ax.set_xlim(0, 1), ax.set_xticks([])
     ax.set_ylim(0, 1), ax.set_yticks([])
 
-def update(frame):
-    global size
-    # Each ring is made larger
-    size += (SIZE_MAX - SIZE_MIN) / NS
+    # Set time
+    day = 0
+    time = 0
 
-    # line = ax.plot([0.5,0.8],[0.5,0.7], color ='red', linewidth=1.5, linestyle="-")
-    # line.set_data([0.3,0.3],[0.5,0.8])
-    # line.set_linewidth(LINE_MIN)
+def update(frame):
+    global size, time, day
+    # Each ring is made larger
+    # size += (SIZE_MAX - SIZE_MIN) / NS
+
+    # Set line widths
+    # for i in range(NS):
+    #     for j in range(NS):
+    line[1].set_linewidth(10)
 
     # Reset specific ring
-    # i = frame % NS
-    # size[i] = SIZE_MIN
-    if frame % 50 == 0:
-        for i in range(NS):
-            size[i] = SIZE_MIN
+    for i in range(NS):
+        o_size = size[i] #/ 2 #(size[i] - 50) / 4.5
+        n_size = o_size - sum([DATAS[day][time][i][j] for j in range(NS)]) \
+                + sum([DATAS[day][time][j][i] for j in range(NS)])
+        if n_size < 0:
+            print(n_size)
+        size[i] = n_size #* 2 # * 4.5 + 50
 
     # Update scatter object
     # scat.set_edgecolors(color)
     scat.set_sizes(size)
     scat.set_offsets(pos)
+
+    time += 1
+    if time >= 48:
+        time = 0
+        day += 1
+    if day >= 10:
+        day = 0
+        size = np.linspace(SIZE_INI, SIZE_INI, NS)
 
     # Return the modified object
     objects = tuple(line + [scat])
@@ -78,7 +96,14 @@ def showAnimation():
     anim = animation.FuncAnimation(fig, update, interval = 50, blit = True, frames = 200)
     plt.show()
 
+def get_transdata():
+    with open('data/transports') as f:
+        for line in f.readlines():
+            data = eval(line.replace('\n', ''))
+            DATAS.append(data)
+
 def main():
+    get_transdata()
     showAnimation()
 
 if __name__ == '__main__':
